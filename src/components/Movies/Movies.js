3c922cf81e68e89dfa13893c0cedeb4fc25aff74
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-06 09:49:02 +0300

    feat: add sticky footer

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import './Movies.css';
 import Header from '../Header/Header';
+import Navigation from '../Navigation/Navigation';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import MoviesCard from '../MoviesCard/MoviesCard';
+import LazyLoadingButton from '../LazyLoadingButton/LazyLoadingButton';
 import Footer from '../Footer/Footer';
+import BurgerPopup from '../BurgerPopup/BurgerPopup';
 
 function Movies() {
-    return (
-      <div className="movies">
-        <Header />
+  const [burgerClicked, setBurgerClicked] = React.useState(false);
+
+  function handleBurgerIconClick() {
+    setBurgerClicked(true);
+  }
+
+  function handleCloseIconClick() {
+    setBurgerClicked(false);
+  }
+
+  return (
+    <div className="movies">
+      <Header>
+        <Navigation onBurgerButtonClick={handleBurgerIconClick} />
+      </Header>
+      <div className="movies__content">
         <SearchForm />
         <Preloader />
         <MoviesCardList />
-        <MoviesCard />
-        <Footer />
+        <LazyLoadingButton />
       </div>
-    );
+      <Footer />
+      { burgerClicked && <BurgerPopup onCloseIconClick={handleCloseIconClick} /> }
+    </div>
+  );
 }
   
 export default Movies;
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './SavedMovies.css';
 import Header from '../Header/Header';
-import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import MoviesCard from '../MoviesCard/MoviesCard';
+import Navigation from '../Navigation/Navigation';
+import SearchForm from '../SearchForm/SearchForm';
+// import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
 
 function SavedMovies() {
     return (
       <div className="saved-movies">
-        <Header />
-        <MoviesCardList />
-        <MoviesCard />
+        <Header>
+          <Navigation />
+        </Header> 
+        <div className="saved-movies__content">
+          <SearchForm />
+          {/* <MoviesCardList />*/}
+        </div>
         <Footer />
       </div>
     );
