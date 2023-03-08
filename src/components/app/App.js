ab7dfbd0d2cb47cb5a53be406bf313cef9af15dd
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-08 12:34:55 +0300

    feat: layout of 404-page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,18 +7,22 @@ import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Main from '../Main/Main';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/signup"><Register /></Route>
-        <Route path="/signin"><Login /></Route>
-        <Route path="/movies"><Movies /></Route>
-        <Route path="/saved-movies"><SavedMovies /></Route>
-        <Route path="/profile"><Profile /></Route>
-        <Route exact path="/"><Main /></Route>
-      </Routes>
+    <div className="app">
+      <div className="app__container">
+        <Routes>
+          <Route path="/signup" element={<Register />} />
+          <Route path="/signin" element={<Login />} />
+          <Route exact path="/movies" element={<Movies />} />
+          <Route path="/saved-movies" element={<SavedMovies />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route exact path="/" element={<Main />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
