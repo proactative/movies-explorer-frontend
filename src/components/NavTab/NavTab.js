a97626090ee445abcda66a6344943b9e71b83510
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-08 12:48:21 +0300

    feat: add NavTab

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -4,7 +4,11 @@ import './NavTab.css';
 function NavTab() {
     return (
       <div className="nav-tab">
-         
+        <ul className="nav-tab__links">
+          <li><a className="nav-tab__link" href="#about-project">О проекте</a></li>
+          <li><a className="nav-tab__link" href="#techs">Технологии</a></li>
+          <li><a className="nav-tab__link" href="#about-me">Студент</a></li>
+        </ul>
       </div>
     );
 }
