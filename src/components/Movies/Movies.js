93cb7f517b8e4878db6a0172117cc0d9ec077d5c
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-08 12:09:25 +0300

    feat: add burger menu popup (with smooth appearing)

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import './Movies.css';
-import Header from '../Header/Header';
-import Navigation from '../Navigation/Navigation';
-import SearchForm from '../SearchForm/SearchForm';
-import Preloader from '../Preloader/Preloader';
-import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import LazyLoadingButton from '../LazyLoadingButton/LazyLoadingButton';
-import Footer from '../Footer/Footer';
-import BurgerPopup from '../BurgerPopup/BurgerPopup';
+import React from 'react'
+import './Movies.css'
+import Header from '../Header/Header'
+import Navigation from '../Navigation/Navigation'
+import SearchForm from '../SearchForm/SearchForm'
+import Preloader from '../Preloader/Preloader'
+// import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import LazyLoadingButton from '../LazyLoadingButton/LazyLoadingButton'
+import Footer from '../Footer/Footer'
+import BurgerPopup from '../BurgerPopup/BurgerPopup'
 
 function Movies() {
-  const [burgerClicked, setBurgerClicked] = React.useState(false);
+  const [burgerClicked, setBurgerClicked] = React.useState(false)
 
   function handleBurgerIconClick() {
-    setBurgerClicked(true);
+    setBurgerClicked(true)
   }
 
   function handleCloseIconClick() {
-    setBurgerClicked(false);
+    setBurgerClicked(false)
   }
 
   return (
-    <div className="movies">
+    <div className={burgerClicked ? 'movies_fixed-height' : 'movies'}>
       <Header>
         <Navigation onBurgerButtonClick={handleBurgerIconClick} />
       </Header>
       <div className="movies__content">
         <SearchForm />
         <Preloader />
-        <MoviesCardList />
+        {/* <MoviesCardList /> */}
         <LazyLoadingButton />
       </div>
       <Footer />
-      { burgerClicked && <BurgerPopup onCloseIconClick={handleCloseIconClick} /> }
+      <BurgerPopup
+        onCloseIconClick={handleCloseIconClick}
+        className={
+          !burgerClicked ? 'burger-popup' : 'burger-popup burger-popup_opened'
+        }
+      />
     </div>
-  );
+  )
 }
-  
-export default Movies;
+
+export default Movies
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import './SavedMovies.css';
-import Header from '../Header/Header';
-import Navigation from '../Navigation/Navigation';
-import SearchForm from '../SearchForm/SearchForm';
-// import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import Footer from '../Footer/Footer';
+import React from 'react'
+import './SavedMovies.css'
+import Header from '../Header/Header'
+import Navigation from '../Navigation/Navigation'
+import SearchForm from '../SearchForm/SearchForm'
+import MoviesCardList from '../MoviesCardList/MoviesCardList'
+import Footer from '../Footer/Footer'
+import BurgerPopup from '../BurgerPopup/BurgerPopup'
 
 function SavedMovies() {
-    return (
-      <div className="saved-movies">
-        <Header>
-          <Navigation />
-        </Header> 
-        <div className="saved-movies__content">
-          <SearchForm />
-          {/* <MoviesCardList />*/}
-        </div>
-        <Footer />
+  const [burgerClicked, setBurgerClicked] = React.useState(false)
+
+  function handleBurgerIconClick() {
+    setBurgerClicked(true)
+  }
+
+  function handleCloseIconClick() {
+    setBurgerClicked(false)
+  }
+
+  return (
+    <div
+      className={burgerClicked ? 'saved-movies_fixed-height' : 'saved-movies'}
+    >
+      <Header>
+        <Navigation onBurgerButtonClick={handleBurgerIconClick} />
+      </Header>
+      <div className="saved-movies__content">
+        <SearchForm />
+        <MoviesCardList />
       </div>
-    );
+      <Footer />
+      <BurgerPopup
+        onCloseIconClick={handleCloseIconClick}
+        className={
+          !burgerClicked ? 'burger-popup' : 'burger-popup burger-popup_opened'
+        }
+      />
+    </div>
+  )
 }
-  
-export default SavedMovies;
+
+export default SavedMovies
