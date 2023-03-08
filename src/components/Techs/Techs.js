01e1dc6d87902f9551cc0ca00eeec4f5880ad0f4
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-08 16:12:07 +0300

    feat: add Techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './Techs.css';
+import React from 'react'
+import './Techs.css'
+import SectionContainer from '../SectionContainer/SectionContainer'
 
 function Techs() {
-    return (
-      <div className="techs">
-         
-      </div>
-    );
+  return (
+    <div className="techs" id="techs">
+      <SectionContainer title="Технологии">
+        <h2 className="techs__title">7 технологий</h2>
+        <p className="techs__subtitle">
+          На курсе веб-разработки мы освоили технологии, которые применили в
+          дипломном проекте.
+        </p>
+        <ul className="techs__list">
+          <li className="techs__item">HTML</li>
+          <li className="techs__item">CSS</li>
+          <li className="techs__item">JS</li>
+          <li className="techs__item">React</li>
+          <li className="techs__item">Git</li>
+          <li className="techs__item">Express.js</li>
+          <li className="techs__item">mongoDB</li>
+        </ul>
+      </SectionContainer>
+    </div>
+  )
 }
-  
-export default Techs;
+
+export default Techs
