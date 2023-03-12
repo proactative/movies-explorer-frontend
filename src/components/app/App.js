edeb7e69a31ae160f71e1ef80be410ec3e7cf26e
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-12 14:21:16 +0300

    refactor: remove odd div

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Profile from '../Profile/Profile';
-import Main from '../Main/Main';
-import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
+import Movies from '../Movies/Movies'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import Profile from '../Profile/Profile'
+import Main from '../Main/Main'
+import NotFoundPage from '../NotFoundPage/NotFoundPage'
 
 function App() {
   return (
     <div className="app">
-      <div className="app__container">
-        <Routes>
-          <Route path="/signup" element={<Register />} />
-          <Route path="/signin" element={<Login />} />
-          <Route exact path="/movies" element={<Movies />} />
-          <Route path="/saved-movies" element={<SavedMovies />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route exact path="/" element={<Main />} />
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/signup" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
+        <Route exact path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route exact path="/" element={<Main />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
