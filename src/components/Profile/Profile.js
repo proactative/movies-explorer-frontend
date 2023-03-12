c60d53eab46f017efb412247459e61ec7e66bac4
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-12 16:44:30 +0300

    feat: add validation and save-button to Profile Page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,6 +7,53 @@ import BurgerPopup from '../BurgerPopup/BurgerPopup'
 
 function Profile() {
   const [burgerClicked, setBurgerClicked] = React.useState(false)
+  const [editMode, setEditMode] = React.useState(false)
+  const [isActiveInput, setIsActiveInput] = React.useState('disabled')
+  const [isEmailInputValid, setIsEmailInputValid] = React.useState(true)
+  const [isUserInputValid, setIsUserInputValid] = React.useState(true)
+  const [userInputErrorMessage, setUserInputErrorMessage] = React.useState('')
+  const [emailInputErrorMessage, setEmailInputErrorMessage] = React.useState('')
+
+  // Виталий and pochta@yandex.ru is a temprory decision only for level-2
+  const [user, setUser] = React.useState('Виталий')
+  const[email, setEmail] = React.useState('pochta@yandex.ru')
+  const [isFormValid, setIsFormValid] = React.useState(false)
+
+  function handleUserInputChange(e) {
+    if (e.target.validity.valid) {
+      setIsUserInputValid(true)
+      setUserInputErrorMessage('')
+    } else {
+      setIsUserInputValid(false)
+      setUserInputErrorMessage(e.target.validationMessage)
+    }
+    setUser(e.target.value);
+  }
+
+  function handleEmailInputChange(e) {
+    if (e.target.validity.valid) {
+      setIsEmailInputValid(true)
+      setEmailInputErrorMessage('')
+    } else {
+      setIsEmailInputValid(false)
+      setEmailInputErrorMessage(e.target.validationMessage)
+    }
+    setEmail(e.target.value);
+  }
+
+  React.useEffect(() => {
+    setIsFormValid(isUserInputValid && isEmailInputValid)
+  }, [isUserInputValid, isEmailInputValid])
+ 
+  function switchOnEditMode() {
+    setEditMode(true);
+    setIsActiveInput('');
+  }
+
+  function switchOffEditMode() {
+    setEditMode(false);
+    setIsActiveInput('disabled')
+  }
 
   function handleBurgerIconClick() {
     setBurgerClicked(true)
@@ -21,7 +68,7 @@ function Profile() {
       <Header>
         <Navigation onBurgerButtonClick={handleBurgerIconClick} />
       </Header>
-      <h2 className="profile__title">Привет,&nbsp;Друг!</h2>
+      <h2 className="profile__title">Привет,&nbsp;{user}!</h2>
       <form className="profile__form">
         <fieldset className="profile__fieldset">
           <label className="profile__label">
@@ -34,9 +81,11 @@ function Profile() {
               name="name"
               id="name"
               required
-              value="Виталий"
+              value={ "" || user}
+              disabled={isActiveInput}
+              onChange={handleUserInputChange}
             />
-            <span className="profile__error-span"></span>
+            <span className="profile__error-span">{userInputErrorMessage}</span>
           </label>
           <label className="profile__label">
             E&#8209;mail
@@ -45,20 +94,34 @@ function Profile() {
               type="email"
               id="email"
               required
-              value="pochta@yandex.ru"
+              value={ "" || email}
+              pattern='^.*@.+$'
+              disabled={isActiveInput}
+              onChange={handleEmailInputChange}
             />
-            <span className="profile__error-span"></span>
+            <span className="profile__error-span">{emailInputErrorMessage}</span>
           </label>
         </fieldset>
-        <button className="profile__form-button" type="button">
+        {!editMode &&
+        <button className="profile__form-button" type="button" onClick={switchOnEditMode}>
           Редактировать
-        </button>
+        </button> }
+        {editMode &&
+         <button
+          onClick={switchOffEditMode}
+          type="submit"
+          className={isFormValid ? "profile__submit-button" : "profile__submit-button profile__submit-button_disabled" }
+          disabled={isFormValid ? "" : "disabled"}
+          >
+          Сохранить
+          </button>}
       </form>
 
       <div className="profile__redirection-link-area">
+      {!editMode &&
         <Link className="profile__redirection-link" to="/">
           Выйти из аккаунта
-        </Link>
+        </Link> }
       </div>
       <BurgerPopup
         onCloseIconClick={handleCloseIconClick}
