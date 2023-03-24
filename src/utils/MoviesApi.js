66833df6827c6a336105d7a9ca4917ab3a174b57
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-24 11:23:12 +0300

    feat: add MoviesApi

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,19 @@
+export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies'
+
+function getResponseData(res) {
+  if (!res.ok) {
+    return Promise.reject(res)
+  }
+  return res.json()
+}
+
+export const getAllMovies = () => {
+  return fetch(`${MOVIES_URL}/`, {
+    headers: {
+      'Content-Type': 'application/json',
+
+    },
+  }).then(getResponseData)
+}
+
+ // Authorization: `Bearer ${localStorage.getItem('jwt')}`,
