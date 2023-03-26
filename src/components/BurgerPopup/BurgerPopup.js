9eb593921d009056ab073c259ddac606deb4b766
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2023-03-26 14:54:15 +0300

    feat: add closing BurgerPopup via click on the link

--- src/components/BurgerPopup/BurgerPopup.js
+++ src/components/BurgerPopup/BurgerPopup.js
@@ -8,18 +8,18 @@ function BurgerPopup(props) {
   return (
     <div className={props.className}>
       <div className="burger-popup__menu">
-        <button className="burger-popup__close-button" type='button'  onClick={props.onCloseIconClick}/>
+        <button className="burger-popup__close-button" type='button' onClick={props.onCloseIconClick} />
         <nav className="burger-popup__links">
-          <NavLink to="/" className="burger-popup__link">
+          <NavLink to="/" className="burger-popup__link"  onClick={props.onCloseIconClick}>
             <p className="burger-popup__link-text">Главная</p>
           </NavLink>
-          <NavLink to="/movies" className="burger-popup__link">
+          <NavLink to="/movies" className="burger-popup__link"  onClick={props.onCloseIconClick}>
             <p className={location.pathname === "/movies" ? "burger-popup__link-text active-link" : "burger-popup__link-text"}>Фильмы</p>
           </NavLink>
-          <NavLink to="/saved-movies" className="burger-popup__link">
+          <NavLink to="/saved-movies" className="burger-popup__link"  onClick={props.onCloseIconClick}>
             <p className={location.pathname === "/saved-movies" ? "burger-popup__link-text active-link" : "burger-popup__link-text"}>Сохранённые фильмы</p>
           </NavLink>
-          <NavLink to="/profile" className="burger-popup__link">
+          <NavLink to="/profile" className="burger-popup__link" onClick={props.onCloseIconClick}>
             <p className={location.pathname === "/profile" ? "burger-popup__link-text active-link" : "burger-popup__link-text"}>Аккаунт</p>
             <span className="burger-popup__span" />
           </NavLink>
